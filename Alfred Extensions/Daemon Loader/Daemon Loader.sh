ffce2c93ce47a99a5a5b6aa74a2300260f76ae67
Jonathan Wiesel <jonathanwiesel@gmail.com>
2012-08-10 16:28:28 -0430

    new version with sudo password permission

--- Alfred Extensions/Daemon Loader/Daemon Loader.sh
+++ Alfred Extensions/Daemon Loader/Daemon Loader.sh
@@ -1,14 +1,29 @@
+sudo -k
+action=$(echo {query} | cut -d " " -f 1)
+pass=$(echo {query} | cut -d " " -f 2)
+
 ON="load"
 OFF="unload"
 
 daemon="/Library/LaunchDaemons/net.conceited.RubbernetDaemon.plist"
 
-if [ {query} = $ON ]; then
-	sudo launchctl load -w "${daemon}"
-echo "Daemon Loaded"
-fi
-
-if [ {query} = $OFF ]; then
-sudo launchctl unload -w "${daemon}"
-echo "Daemon Unloaded"
+if [ $action = $ON ]; then
+	echo "$pass" | sudo -S launchctl load -w "${daemon}"
+	if [ "$?" -eq 0 ]; then
+		echo "Daemon Loaded"
+		sudo -k
+	else
+		echo "wrong pass or daemon already loaded"
+	fi
+elif [ $action = $OFF ]; then
+	echo "$pass" | sudo -S launchctl unload -w "${daemon}"
+	if [ "$?" -eq 0 ]; then
+		echo "Daemon Unloaded"
+		sudo -k
+	else
+		echo "wrong pass or daemon not present"
+	fi
+else
+	echo Command "{query}" not valid
+	echo Use Load or Unload
 fi
--- Alfred Extensions/Daemon Loader/README.md
+++ Alfred Extensions/Daemon Loader/README.md
@@ -9,13 +9,7 @@ Then you MUST specify the path to the daemon in the *"daemon"* variable
 
 *daemon="/path/to/daemon/"*
 
-To run just type *"daemon load"* or *"daemon unload"* in Alfred bar.
-
-This extension may not work for you because it uses the *"sudo"* command without a password
-
-If you want this extensions to work, you'll need to disable the *"sudo"* password request. Without the *"sudo"* command is not possible to load or unload daemons. 
-
-You can follow the steps to disable the *sudo* password request **[from here][macworld]**
+To run just type *"daemon load password"* or *"daemon unload password"* in Alfred bar.
 
 
-[macworld]: http://hints.macworld.com/article.php?story=20021202054815892 "LINK"
+***Note that your password is needed because in order to load or unload daemons sudo permissions are required***
