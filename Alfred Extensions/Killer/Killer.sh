24052d814913d8c645facf3169100acbcd767a49
Jonathan Wiesel <jonathanwiesel@gmail.com>
2012-08-16 16:53:15 -0430

    path to process added so no exact match need to be found to kill application

--- Alfred Extensions/Killer/Killer.sh
+++ Alfred Extensions/Killer/Killer.sh
@@ -2,9 +2,11 @@
 p={query}
 number=$(ps aux | grep -i "${p}" | grep -v grep | wc -l)
 
+ruta=$(ps -A | grep -i "${p}" | grep -v grep | awk '{$1=$2=$3=""; print  $0 }' | awk -F.app '{ print $1 }' | uniq | awk -F/ '{print $NF}')
+
 if [[ $number -gt 0 ]]
 then
-	sudo killall "${p}" 
+	killall "${ruta}" 
 	echo ""${p}" - Process killed";
 else
 	echo ""${p}" - Not Running"
